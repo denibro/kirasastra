041c6aee8f06909387067afd431a4db945912d3c
denibro <denisdewdew@gmail.com>
2021-02-10 19:07:28 +0700

    landingpage area pupuler area terbaru

--- resources/views/landingpages/part/index.blade.php
+++ resources/views/landingpages/part/index.blade.php
@@ -6,18 +6,15 @@
 
   <div class="container">
     <div class="card area_populer">
-      <div class="card-body">
         <div class="card-deck">
           <div class="card s">
-            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Judul</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.</p>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deleniti aperiam tenetur necessitatibus excepturi exercitationem, explicabo deserunt. Nihil quaerat, alias porro molestias fugiat vel numquam reprehenderit illum qui. Nobis, minus.</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
           </div>
           <div class="card s">
-            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Judul</h5>
               <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -25,7 +22,6 @@
             </div>
           </div>
           <div class="card s">
-            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Judul</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -33,6 +29,16 @@
             </div>
           </div>
         </div>
+      <div class="text-terbaru">
+          <h5>Terbaru</h5>
       </div>
+      <div class=" card area_terbaru">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor sapiente optio quidem! Dolorum, in porro. Deleniti amet illo rem autem tempora. Corporis nam quae odio ipsum maiores eius, earum natus?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error amet iste vero, nisi reiciendis eum doloremque, libero tempore aspernatur quia, blanditiis obcaecati commodi quidem eveniet sapiente repellendus necessitatibus dicta mollitia.
+      </div>
+      <div class=" card area_terbaru">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor sapiente optio quidem! Dolorum, in porro. Deleniti amet illo rem autem tempora. Corporis nam quae odio ipsum maiores eius, earum natus?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error amet iste vero, nisi reiciendis eum doloremque, libero tempore aspernatur quia, blanditiis obcaecati commodi quidem eveniet sapiente repellendus necessitatibus dicta mollitia.
+      </div>    
     </div>
   </div>
